77df8fafc0ad47041f050eec7b4a1ed14b31e5d1
Tasman Leach <tasman@tasmanleach.com>
2023-06-25 12:37:01 +1000

    Minor text change

--- src/AUSBird.DiscordBot/AUSBird.DiscordBot.Abstraction/Modules/Interactions/IDiscordComponentInteraction.cs
+++ src/AUSBird.DiscordBot/AUSBird.DiscordBot.Abstraction/Modules/Interactions/IDiscordComponentInteraction.cs
@@ -5,5 +5,5 @@ namespace AUSBird.DiscordBot.Abstraction.Modules.Interactions;
 public interface IDiscordComponentInteraction : IDiscordInteraction
 {
     public string[] ComponentIds { get; }
-    public Task ExecuteInteractionAsync(IComponentInteraction modal);
+    public Task ExecuteInteractionAsync(IComponentInteraction component);
 }
